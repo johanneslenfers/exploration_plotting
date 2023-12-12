f5a43e883fb0fb26127d46cd90d4a46f03a4e822
Johannes Lenfers <j.le@wwu.de>
2023-12-12 20:07:03 +0100

    updated scatter plottign

--- exploration_plotting/scatter.py
+++ exploration_plotting/scatter.py
@@ -12,8 +12,14 @@ import numpy as np
 def scatter(plotting_configuration: PlottingConfiguration) -> None:
     data = util.get_data_fully(plotting_configuration)
 
-    y = []
     for method in data:
+
+        layout = go.Layout(title=f'Scatter Plot - {method}',
+                           xaxis=dict(title='Samples'),
+                           yaxis=dict(title='Log Runtime (ms)'))
+
+        y = []
+        print(f"method: {method}")
         for run in data[method][1]:
 
             maximum = max([float(elem['runtime']) for elem in data[method][1][run]])
@@ -25,20 +31,13 @@ def scatter(plotting_configuration: PlottingConfiguration) -> None:
                 else:
                     y.append(np.log10(float(line['runtime'])))
 
-    x = list(range(len(y)))
+        x = list(range(len(y)))
 
-    # Create trace for scatter plot
-    trace = go.Scatter(x=x, y=y, mode='markers', marker=dict(size=1))
+        trace = go.Scatter(x=x, y=y, mode='markers', marker=dict(size=0.5, color='black'))
 
-    # Create layout for the plot
-    layout = go.Layout(title='Simple Scatter Plot',
-                       xaxis=dict(title='X-axis'),
-                       yaxis=dict(title='Y-axis'))
+        fig = go.Figure(data=trace, layout=layout)
 
-    # Create figure and add trace and layout
-    fig = go.Figure(data=trace, layout=layout)
-
-    # Show the figure
-    fig.show()
+        fig.write_image(
+            f"{plotting_configuration.output}/{plotting_configuration.name}_{method}_scatter.pdf")
 
     return None
